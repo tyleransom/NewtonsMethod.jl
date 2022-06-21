327f633e20f15c9e64458e37d11256640b017501
Tyler Ransom <tyleransom@gmail.com>
2022-06-21 12:33:16 -0500

    Adding tests

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,36 @@
 module NewtonsMethod
 
-# Write your package code here.
+using ForwardDiff, LinearAlgebra
+
+function newton_root(f, f_prime; x0 = 0, tolerance=1E-7, maxiter=1_000)
+    # setup the algorithm
+    x_old = x0
+    normdiff = Inf
+    iter = 1
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_old - f(x_old)/f_prime(x_old)
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter += 1
+    end
+    return (value=x_old, normdiff=normdiff, iter=iter) # A named tuple
+end
+
+function newton_root_AD(f; x0 = 0, tolerance=1E-7, maxiter=1_000)
+    # setup the algorithm
+    x_old = x0
+    normdiff = Inf
+    iter = 1
+    f_prime = x -> ForwardDiff.derivative(f, x)
+    while normdiff > tolerance && iter <= maxiter
+        x_new = x_old - f(x_old)/f_prime(x_old)
+        normdiff = norm(x_new - x_old)
+        x_old = x_new
+        iter += 1
+    end
+    return (value=x_old, normdiff=normdiff, iter=iter) # A named tuple
+end
+
+export newton_root, newton_root_AD
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,40 @@
 using NewtonsMethod
 using Test
 
-@testset "NewtonsMethod.jl" begin
-    # Write your tests here.
+@testset "(x-1)^3 Analytical" begin
+    f(x)  = (x-1)^3
+    df(x) = 3*(x-1)^2
+    out = newton_root(f, df)
+    @test isapprox(out.value, 1, rtol=1e-6)
+    @test out.iter < 1_000
 end
+
+@testset "(x-1)^3 AutoDiff" begin
+    f(x) = (x-1)^3
+    out = newton_root_AD(f)
+    @test isapprox(out.value, 1, rtol=1e-6)
+    @test out.iter < 1_000
+end
+
+@testset "(x-π)^2 Analytical" begin
+    f(x)  = (x-π)^2
+    df(x) = 2*(x-π)
+    out = newton_root(f, df)
+    @test isapprox(out.value, π, rtol=1e-6)
+    @test out.iter < 1_000
+end
+
+@testset "(e^x)-2 AutoDiff" begin
+    f(x) = exp(x)-2
+    out = newton_root_AD(f)
+    @test isapprox(out.value, log(2), rtol=1e-6)
+    @test out.iter < 1_000
+end
+
+@testset "(x-3)^5 AutoDiff" begin
+    f(x) = (x-3)^5
+    out = newton_root_AD(f)
+    @test isapprox(out.value, 3, rtol=1e-6)
+    @test out.iter < 1_000
+end
+
